868ac6e286090bd2433944d8a7ca7ce6a2bd331a
bjorn jongenelen <45263124+BjornJongenelen@users.noreply.github.com>
2021-12-02 13:58:40 +0100

    opdracht 1B easy upload

---
+++ Opdracht 1B easy/easy_start/easy_start/Program.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+DateTime Date = DateTime.Now;
+DateTime Datum1 = DateTime.Now;
+DateTime Datum2 = DateTime.Now;
+
+var client = new HttpClient();
+client.BaseAddress = new Uri("http://involved-htf-challenge.azurewebsites.net");
+var token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiNDEiLCJuYmYiOjE2Mzg0Mzc3NTksImV4cCI6MTYzODUyNDE1OSwiaWF0IjoxNjM4NDM3NzU5fQ.yPfhm_tn9_CIfr6Le0DV68QIf5Zi0SQy8HfSefW7iN1znYnP9J8Yoz4yElLkc207e4Mv96eFFzm9uCzioRW5zw";
+client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+var startUrl = "/api/path/2/easy/Start";
+var startResponse = await client.GetAsync(startUrl);
+var sampleUrl = "/api/path/2/easy/Sample";
+var sampleGetResponse = await client.GetStringAsync(sampleUrl);
+
+Console.WriteLine(sampleGetResponse);
+Datum(10, sampleGetResponse);
+Datum1 = Date;
+Datum(37, sampleGetResponse);
+Datum2 = Date;
+
+var dateinseconds = (Datum2 - Datum1).TotalSeconds *-1;
+Console.WriteLine(dateinseconds);
+
+void Datum(int teller, string datum)
+{
+    string date = Convert.ToString(datum);
+
+    var Day = "";
+    var Month = "";
+    var Year = "";
+
+    for (int i = 0; i < 4; i++)
+    {
+        string datedeel = date.Substring(teller, 4);
+        string dateCheck = date.Substring(teller + 2, 2);
+        teller += 4;
+
+        if (dateCheck == "DD")
+        {
+            Day = datedeel;
+            Day = Day.Substring(0, 2);
+        }
+        else if (dateCheck == "MM")
+        {
+            Month = datedeel;
+            Month = Month.Substring(0, 2);
+        }
+        else if (dateCheck == "YY")
+        {
+            var Trash = datedeel;
+        }
+        else
+        {
+            Year = datedeel;
+        }
+
+    }
+
+    Console.WriteLine(Day + " " + Month + " " + Year);
+    var fulldatum = Day + "/" + Month + "/" + Year;
+    var dateTime = DateTime.Parse(fulldatum);
+    Console.WriteLine(dateTime);
+    Date = dateTime;
+
+}
+
+
+//api url /api/path/1/easy/Puzzle
+
+//code to get data from the api
